6a613cdf69664e8f86e514641880361f4560a4a2
EC2 Default User <g1621362@gmail.com>
2021-01-19 13:07:11 +0000

    [update] test

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 describe '投稿のテスト' do
-  let!(:list){create(:book,title:'title',body:'body')}
+  let!(:list){create(:list,title:'title',body:'body')}
+  
   describe 'トップ画面のテスト' do
     before do
       visit top_path
@@ -12,16 +13,92 @@ describe '投稿のテスト' do
       it 'トップ画面に「ここはTopページです」が表示されているか' do
         expect(page).to have_content 'ここはTopページです'
       end
-      it 'top_pathが"/top"であるか'
+      it 'top_pathが"/top"であるか' do
         expect(current_path).to eq('/top')
+      end
     end
   end
+  
   describe '投稿画面のテスト' do
     before do
-      visit todolist_new_path
+      visit todolists_new_path
     end
     context '表示の確認' do
-      it '投稿ボタンが表示されているか'
+      it 'todolists_new_pathが"/todolists/new"であるか' do
+        expect(current_path).to eq('/todolists/new')
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:5)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:10)
+        click_button '投稿'
+        expect(page).to have_current_path todolist_path(List.last)
+      end
+    end
+  end
+  
+  describe '一覧画面のテスト' do
+    before do
+      visit todolists_path
+    end
+    context '一覧画面の表示とリンクの確認' do
+      it '一覧表示画面に投稿されたものが表示されているか' do
+        expect(page).to have_content list.title
+        expect(page).to have_link list.title
+      end
+    end
+  end
+  
+  describe '詳細画面のテスト' do
+    before do
+      visit todolist_path(list)
+    end
+    context '表示のテスト' do
+      it '削除リンクが存在しているか' do
+        expect(page).to have_link '削除'
+      end
+      it '編集リンクが存在しているか' do
+        expect(page).to have_link '編集'
+      end
+    end
+    context 'リンクの遷移先の確認' do
+      it '編集の遷移先は編集画面か' do
+        edit_link = find_all('a')[0]
+        edit_link.click
+        expect(current_path).to eq('/todolists/'+list.id.to_s+'/edit')
+      end
+    end
+    context 'list削除のテスト'do
+      it 'list削除' do
+        expect{list.destroy}.to change{List.count}.by(-1)
+      end
+    end
+  end
+  
+  describe '編集画面のテスト' do
+    before do
+      visit edit_todolist_path(list)
+    end
+    context '表示の確認' do
+      it '編集前のタイトルと本文がフォームに表示されている' do
+        expect(page).to have_field 'list[title]', with: list.title
+        expect(page).to have_field 'list[body]', with: list.body
+      end
+      it '保存ボタンが表示される' do
+        expect(page).to have_button '保存'
+      end
+    end
+    context '更新処理に関するテスト' do
+      it  '更新後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:5)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:10)
+        click_button '保存'
+        expect(page).to have_current_path todolist_path(list)
+      end
     end
   end
 end
