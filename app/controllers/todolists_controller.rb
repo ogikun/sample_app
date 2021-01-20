33887d2c3720952506f4cf0771235563e0119686
EC2 Default User <g1621362@gmail.com>
2021-01-20 07:47:12 +0000

    [Update] dotenv-rails

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,30 +3,31 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
-  def create 
+  def create
     list = List.new(list_params)
     list.save
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
+    puts "作成したキー #{ENV['SECRET_KEY']}"
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
@@ -34,6 +35,7 @@ class TodolistsController < ApplicationController
   end
 
   private
+
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -1,14 +1,15 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'rails_helper'
 
 describe '投稿のテスト' do
-  let!(:list){create(:list,title:'title',body:'body')}
-  
+  let!(:list) { create(:list, title: 'hoge', body: 'body') }
+
   describe 'トップ画面のテスト' do
     before do
       visit top_path
     end
+
     context '表示の確認' do
       it 'トップ画面に「ここはTopページです」が表示されているか' do
         expect(page).to have_content 'ここはTopページです'
@@ -18,11 +19,12 @@ describe '投稿のテスト' do
       end
     end
   end
-  
+
   describe '投稿画面のテスト' do
     before do
       visit todolists_new_path
     end
+
     context '表示の確認' do
       it 'todolists_new_pathが"/todolists/new"であるか' do
         expect(current_path).to eq('/todolists/new')
@@ -31,20 +33,22 @@ describe '投稿のテスト' do
         expect(page).to have_button '投稿'
       end
     end
+
     context '投稿処理のテスト' do
       it '投稿後のリダイレクト先は正しいか' do
-        fill_in 'list[title]', with: Faker::Lorem.characters(number:5)
-        fill_in 'list[body]', with: Faker::Lorem.characters(number:10)
+        fill_in 'list[title]', with: Faker::Lorem.characters(number: 5)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number: 10)
         click_button '投稿'
         expect(page).to have_current_path todolist_path(List.last)
       end
     end
   end
-  
+
   describe '一覧画面のテスト' do
     before do
       visit todolists_path
     end
+
     context '一覧画面の表示とリンクの確認' do
       it '一覧表示画面に投稿されたものが表示されているか' do
         expect(page).to have_content list.title
@@ -52,11 +56,12 @@ describe '投稿のテスト' do
       end
     end
   end
-  
+
   describe '詳細画面のテスト' do
     before do
       visit todolist_path(list)
     end
+
     context '表示のテスト' do
       it '削除リンクが存在しているか' do
         expect(page).to have_link '削除'
@@ -65,24 +70,27 @@ describe '投稿のテスト' do
         expect(page).to have_link '編集'
       end
     end
+
     context 'リンクの遷移先の確認' do
       it '編集の遷移先は編集画面か' do
         edit_link = find_all('a')[0]
         edit_link.click
-        expect(current_path).to eq('/todolists/'+list.id.to_s+'/edit')
+        expect(current_path).to eq('/todolists/' + list.id.to_s + '/edit')
       end
     end
-    context 'list削除のテスト'do
+
+    context 'list削除のテスト' do
       it 'list削除' do
-        expect{list.destroy}.to change{List.count}.by(-1)
+        expect { list.destroy }.to change(List, :count).by(-1)
       end
     end
   end
-  
+
   describe '編集画面のテスト' do
     before do
       visit edit_todolist_path(list)
     end
+
     context '表示の確認' do
       it '編集前のタイトルと本文がフォームに表示されている' do
         expect(page).to have_field 'list[title]', with: list.title
@@ -92,10 +100,11 @@ describe '投稿のテスト' do
         expect(page).to have_button '保存'
       end
     end
+
     context '更新処理に関するテスト' do
-      it  '更新後のリダイレクト先は正しいか' do
-        fill_in 'list[title]', with: Faker::Lorem.characters(number:5)
-        fill_in 'list[body]', with: Faker::Lorem.characters(number:10)
+      it '更新後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number: 5)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number: 10)
         click_button '保存'
         expect(page).to have_current_path todolist_path(list)
       end
